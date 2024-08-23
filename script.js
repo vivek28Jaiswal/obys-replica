cdb296b6ce126235d79ac2769a7a05b9e3063f91
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-08-23 18:30:16 +0530

    gooey effect added!

--- script.js
+++ script.js
@@ -1,97 +1,157 @@
 function smooths() {
-    gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector(".main"),
-        smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy(".main", {
-        scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-    });
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }
 function loader() {
-    var tl = gsap.timeline()
-    tl.from(".line h1", {
-        y: 150,
-        stagger: .25,
-        duration: .6,
-        delay: .5
-    });
-    tl.from(".countdown", {
-        opacity: 0,
-        onStart: function () {
-            h5 = document.querySelector(".countdown h5");
-            var time = 0;
-            setInterval(function () {
-                if (time < 100) {
-                    h5.innerHTML = time++
-                } else {
-                    h5.innerHTML = time
-                }
-            }, 28)
-        }
-    }, "same")
-    tl.to(".line h2", {
-        animationName: "nowAnime",
-        opacity: 1,
-        duration: .5,
-    }, "same")
-    tl.to(".loader", {
-        opacity: 0,
-        duration: .4,
-        delay: 3,
-    })
-    tl.from(".page1", {
-        y: 1500,
-        // opacity: 0,
-        duration: .65,
-        ease: "power4.out",
-    })
-    tl.to(".loader", {
-        display: "none",
-    })
-    tl.from("nav", {
-        opacity: 0,
-    })
-    tl.from(".upText h1", {
-        y: 120,
-        stagger: .25,
-        duration: .58,
-        // opacity:1
-    })
-    tl.from(".zero-one, .page2",{
-        opacity:0
-    },"-=2")
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from(
+    ".countdown",
+    {
+      opacity: 0,
+      onStart: function () {
+        h5 = document.querySelector(".countdown h5");
+        var time = 0;
+        setInterval(function () {
+          if (time < 100) {
+            h5.innerHTML = time++;
+          } else {
+            h5.innerHTML = time;
+          }
+        }, 28);
+      },
+    },
+    "same"
+  );
+  tl.to(
+    ".line h2",
+    {
+      animationName: "nowAnime",
+      opacity: 1,
+      duration: 0.5,
+    },
+    "same"
+  );
+  tl.to(".loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 3,
+  });
+  tl.from(".page1", {
+    y: 1500,
+    // opacity: 0,
+    duration: 0.65,
+    ease: "power4.out",
+  });
+  tl.to(".loader", {
+    display: "none",
+  });
+  tl.from("nav", {
+    opacity: 0,
+  });
+  tl.from(".upText h1", {
+    y: 120,
+    stagger: 0.25,
+    duration: 0.58,
+    // opacity:1
+  });
+  tl.from(
+    ".zero-one, .page2",
+    {
+      opacity: 0,
+    },
+    "-=2"
+  );
 }
 function customCursor() {
-    document.addEventListener("mousemove", function (dets) {
-        gsap.to(".cursor", {
-            left: dets.x,
-            top: dets.y,
-        })
-    })
-    Shery.makeMagnet(".navpart2 h4, .menu-opener__square", {});
+    Shery.mouseFollower({
+        //Parameters are optional.
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+      });
+  Shery.makeMagnet(".navpart2 h4, .menu-opener__square", {});
 }
+function sheryAnimation() {
+  Shery.imageEffect(".img-div", {
+    style: 5 /*OR 5 for different variant */,
+    // debug:true,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: 9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.7917349903853899 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.2, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.69, range: [0, 10] },
+      metaball: { value: 0.56, range: [0, 2] },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.3, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+    gooey: true,
+  });
+}
+
+sheryAnimation();
 loader();
 customCursor();
 smooths();
